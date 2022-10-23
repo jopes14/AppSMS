4badf8bd937c4186ad7f752b400a8e47a603922a
jopes14 <61479990+jopes14@users.noreply.github.com>
2022-10-23 18:34:29 -0500

    Create Credentials.py

---
+++ MainSMS/Credentials.py
@@ -0,0 +1,12 @@
+def username():
+    textnowusername = ""
+    return textnowusername
+def sid():
+    sid = ""
+    return sid
+def csrf():
+    csrf = ""
+    return csrf
+def WeatherKey():
+    OpenWeatherMapKey = ""
+    return OpenWeatherMapKey
